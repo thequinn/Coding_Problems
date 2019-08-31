d3a6c37352ebda2c2926e82df1f60a8edb491188
Anna S <annasun.usa@gmail.com>
2019-08-31 22:24:14 +0800

    Passed: 24 Failed: 12

--- Codewars/TheSupermarketQueue.js
+++ Codewars/TheSupermarketQueue.js
@@ -1,54 +1,64 @@
+/*function queueTime(customers, n) {
+  var w = new Array(n).fill(0);
+  for (let t of customers) {
+    console.log('t:', t);
+    let idx = w.indexOf(Math.min(...w));
+    w[idx] += t;
+  }
+  return Math.max(...w);
+}*/
+
+const isZero = (elem, idx, arr)  => { 
+  //console.log("elem:", elem, ", idx:", idx);
+  return elem === 0;
+}
+
 function queueTime(customers, n) {
-  var total = 0;
+  let arr, cus, idx, total = 0;
+  let relay = false;
 
-  if (customers.length <= 0 )
+  if (customers.length === 0) {
     return 0;
-  if (n === 1) {
-    customers.forEach(curr => { total += curr  });
-    return total;
-  }
-
-  var arr = [];
-  var len = customers.length;
-  // dequeue n customers
-  for (let i = 0; i < n && i < len; i++) {
-    arr[i] = customers.shift();
   }
   
-  // find max time a customer needs
-  var max = arr.reduce((a,b) => {
-    return Math.max(a, b);
-  });
-  // find min time a customer needs
-  var min = arr.reduce((a,b) => {
-    return Math.min(a, b);
-  });
-  console.log("max:", max, ", min:", min);
-
-  // x = max - min, replace max by x
-  var x = max - min;
-  var indexOfMax = 0;
-  var i = arr.find((currVal, i) => { 
-    if (currVal === max) {
-      indexOfMax = i;
-      return currVal; 
-    } 
-  });  
-  console.log("i: ", i);
-  arr[i] = x;
-  // save min to total
-  total += min;
-  // dequeue next customer to replace the min
+  if (customers.length >= n) {
+    arr = new Array(n).fill(0);
+  } else {
+    arr = new Array(customers.length).fill(0);
+    relay = true;
+  }
   
+  while (customers.length > 0 || relay) {
+    while ( (idx = arr.findIndex(isZero)) !== -1 ) {
+      if ( (cus = customers.shift()) !== undefined ) {
+        arr[idx] = cus;
+        //console.log("arr[", idx, "]:", arr[idx]);
+      } else {
+        let max = Math.max(...arr);
+        //console.log("max:", max);
+        total += max;
+        //console.log("total+=max:", total);
+        return total;
+      }
+    }
+     
+    let min = Math.min(...arr);
+    arr.forEach((currVal, i) => {
+      arr[i] = currVal - min;
+    });
+    total += min;
+    //console.log("total+=min:", total);
+  }
+  return total;
 }
 
 var re = 0;
 //re = queueTime([], 1)
-//re = queueTime([1,2,3,4], 1)
-
-re = queueTime([2,2,3,3,4,4], 2)
-console.log("re: ", re);
-
-//re = queueTime([1,2,3,4,5], 100)
 //console.log("re: ", re);
+//re = queueTime([1,2,3,4], 1)
+//console.log("re: ", re);
+//re = queueTime([2,2,3,3,4,4], 2)
+//console.log("re: ", re);
+re = queueTime([1,2,3,4,5], 100)
+console.log("re: ", re);
 
