f455901dc0b2a3f20b9e8285b0ea242931288d44
thequinn <>
2019-09-27 22:08:54 +0800

    In Progress

--- LeetCode/142-Linked_List_Cycle_II.js
+++ LeetCode/142-Linked_List_Cycle_II.js
@@ -1,21 +1,6 @@
 /*
 142. Linked List Cycle II
 
-Algorithm used:
--  Tortoise-Hare Algorithm (Floyd's Cycle Detection) 
-
-
-Solution:
-
-(1) 思路：(不用這裡的Code, 用下方的比較簡單)
--- https://leetcode.com/problems/linked-list-cycle-ii/discuss/286362/JavaScript-Solution-with-illustrations-explanation
--- Quinn's 解釋：
-  用LeetCode 141. Linked List Cycle 確認cycle.  再用slow ptr 一步步走，同時
-  head ptr 也一步步走。若這兩個ptrs的數相等，則找到cycle開頭.
-
-(2) Code:
--- https://leetcode.com/problems/linked-list-cycle-ii/discuss/44897/Python-solution-with-comments.
-
 */
 
 var detectCycle = function(head) {
