8cd21c634fdf9fa9759531b359eeeb56600f0f48
thequinn <>
2019-09-27 22:11:28 +0800

    Update

--- LeetCode/142-Linked_List_Cycle_II.js
+++ LeetCode/142-Linked_List_Cycle_II.js
@@ -6,14 +6,7 @@
 var detectCycle = function(head) {
   let fast = head;
   let slow = head;
-  
-  // Not testing fast.next is null will cause error in ln-55:
-  // - ex. If a Linked List is [1] and has no cycle
-  //      -> fast.next returns null: Correct
-  //      -> fast.next.next will be null.next: ERROR
-  //
-  //while (fast) {
-  //
+ 
   while (fast && fast.next) {
     fast = fast.next.next;
     slow = slow.next;
