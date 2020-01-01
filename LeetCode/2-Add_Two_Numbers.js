bdba2ee7cf7be9b21ca5afce9ccbd04ed5dd3f5d
thequinn <annasun.usa@gmail.com>
2020-01-02 02:54:49 +0800

    done

---
+++ LeetCode/2-Add_Two_Numbers.js
@@ -0,0 +1,36 @@
+/*
+2. Add Two Numbers
+*/
+
+var addTwoNumbers = function(l1, l2) {
+  var List = new ListNode(0);
+  var head = List;
+  var sum = 0, carry = 0;
+
+  while (l1! == null || l2! == null || sum > 0) {
+
+    if (l1! == null){
+      sum = sum + l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2! == null){
+      sum = sum + l2.val;
+      l2 = l2.next;
+    }
+
+    if (sum >= 10){
+      carry = 1;
+      sum = sum - 10;
+    }
+
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    sum = carry;
+    carry = 0;
+  }
+
+  return List.next;
+};
+
