5c801cf76a0e67b0aacfed1ebe65350ae3cdf56f
thequinn <annasun.usa@gmail.com>
2020-01-13 02:07:08 +0800

    done

---
+++ Codewars/Replace_With_Alphabet_Position.py
@@ -0,0 +1,8 @@
+
+def alphabet_position(text):
+
+  return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
+
+
+res = alphabet_position("The sunset sets at twelve o' clock.")
+print(res) # "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
