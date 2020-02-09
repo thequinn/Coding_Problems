e50ce9d9ed8dc29dc1acfa14d69be545b916c754
thequinn <annasun.usa@gmail.com>
2020-02-09 01:11:28 -0800

    Update 105-Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.js

--- LeetCode/105-Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.js
+++ LeetCode/105-Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.js
@@ -4,26 +4,23 @@
 */
 
 var buildTree = function(preorder, inorder) {
-  p = i = 0
-
-  build = function(stop) {
-    if (inorder[i] != stop) {
-      var root = new TreeNode(preorder[p++])
-      root.left = build(root.val)
-      i++
-      root.right = build(stop)
-      return root
+    p = i = 0
+    build = function(stop) {
+        if (inorder[i] != stop) {
+            var root = new TreeNode(preorder[p++])
+            root.left = build(root.val)
+            i++
+            root.right = build(stop)
+            return root
+        }
+        return null
     }
-    return null
-  }
-
-  return build()
+    return build()
 };
 
+//------------------------------------
 
-/*
 preorder = [1, 2, 4, 5, 3, 6]
 inorder = [4, 2, 5, 1, 6, 3]
 
-Answer: [3,9,20,null,null,15,7]
-*/
+// Answer: [3,9,20,null,null,15,7]
