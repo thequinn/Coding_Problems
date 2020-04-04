ca09a12c32e234da3e69a754a7a3aa4633fd95ee
thequinn <annasun.usa@gmail.com>
2020-04-04 00:09:00 -0700

    done

---
+++ LeetCode/33-Search_in_Rotated-Sorted_Array.js
@@ -0,0 +1,27 @@
+/*
+33. Search in Rotated Sorted Array
+*/
+
+var search = function(nums, target) {
+  if (nums.length === 0) return -1;
+
+  var index = -1,
+    head = 0,
+    tail = nums.length - 1,
+    mid;
+
+  while (head <= tail) {
+    mid = Math.floor((head + tail) / 2);
+    if (nums[mid] === target) {
+      index = mid;
+      break;
+    } else if ((nums[head] <= target && target < nums[mid]) ||
+               (nums[head] > nums[mid] && (nums[head] <= target || target < nums[mid]))) {
+      tail = mid - 1;
+    } else {
+      head = mid + 1;
+    }
+  }
+
+  return index;
+};
