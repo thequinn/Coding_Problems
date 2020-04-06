7d388597b550ff6f1e5b8b35151cee8ea8ee3811
thequinn <annasun.usa@gmail.com>
2020-04-06 01:28:50 -0700

    done

---
+++ LeetCode/55-Jump_Game.java
@@ -0,0 +1,26 @@
+/*
+55. Jump Game
+
+*/
+public class Solution_1 {
+  public boolean canJumpFromPos(int pos, int[] nums) {
+    if (pos == nums.length - 1) {
+      return true;
+    }
+
+    int furthestJump = Math.min(pos + nums[pos], nums.length - 1);
+
+    for (int nextPos = pos + 1; nextPos <= furthestJump; nextPos++) {
+      if (canJumpFromPos(nextPos, nums)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean canJump(int[] nums) {
+    return canJumpFromPos(0, nums);
+  }
+}
+
+
