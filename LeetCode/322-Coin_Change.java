40b4f35373e1618f4d7288d7ce55981bdc749cbc
thequinn <annasun.usa@gmail.com>
2020-04-23 22:02:56 -0700

    done

--- LeetCode/322-Coin_Change.java
+++ LeetCode/322-Coin_Change.java
@@ -5,8 +5,8 @@ public class Coin_Change {
     int[] coins = {1, 2, 5};    int amount = 11;    // 3
     //int[] coins = {2};          int amount = 3;     // -1
 
-    Solution_1 sol = new Solution_1();
-    int res = sol.coinChange(coins, amount);
+    Solution_1 sol_1 = new Solution_1();
+    int res = sol_1.coinChange(coins, amount);
     System.out.println("\nres:" + res);
   }
 }
