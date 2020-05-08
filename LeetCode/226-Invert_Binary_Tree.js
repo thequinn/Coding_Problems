dc02c9dbcfae59cdc3706013d38b1fbcb4d4a538
thequinn <annasun.usa@gmail.com>
2020-05-07 22:12:17 -0700

    done

---
+++ LeetCode/226-Invert_Binary_Tree.js
@@ -0,0 +1,16 @@
+/*
+226. Invert Binary Tree
+*/
+
+var invertTree = function(root) {
+    if (root == null)   return null;
+
+    let left  = invertTree(root.left);
+    let right = invertTree(root.right);
+    root.right = left;
+    root.left = right;
+
+    return root;
+};
+
+
