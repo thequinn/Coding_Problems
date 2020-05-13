01439439e684e19871e705e9cdadfa52dea98073
thequinn <annasun.usa@gmail.com>
2020-05-13 09:58:18 -0700

    done

---
+++ LeetCode/617-Merge_Two_Binary_Trees.js
@@ -0,0 +1,17 @@
+/*
+617. Merge Two Binary Trees
+
+*/
+
+var mergeTrees = function(t1, t2) {
+  if (t1 == null)   return t2;
+  if (t2 == null)   return t1;
+
+  mergeTrees(t1.left, t2.left);
+  t1.val += t2.val;
+  mergeTrees(t1.right, t2.right);
+  return t1;
+};
+
+
+
