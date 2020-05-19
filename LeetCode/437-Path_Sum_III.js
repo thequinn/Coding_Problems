4b63a7619fea52e0d90d9cb29a2cac89a9c5cf02
thequinn <annasun.usa@gmail.com>
2020-05-18 17:41:50 -0700

    done

--- LeetCode/437-Path_Sum_III.js
+++ LeetCode/437-Path_Sum_III.js
@@ -1,6 +1,22 @@
 /*
 437. Path Sum III
-
 */
 
+const pathSum = (root, sum) => {
+  if (!root)
+    return 0;
+
+  const dfs = (root, sum) => {
+    let count = 0;
+    if (!root)
+      return 0;
+    if (root.val == sum)
+      count +=1;
+    count += dfs(root.left , sum - root.val);
+    count += dfs(root.right, sum - root.val);
+    return count;
+  };
+
+  return dfs(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+};
 
