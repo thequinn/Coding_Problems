2ec9c7d33a476f1bbf5b0c1a813106cfb9fe4613
thequinn <annasun.usa@gmail.com>
2020-06-07 21:31:09 -0700

    Update 53-Maximum_Subarray.js

--- LeetCode/53-Maximum_Subarray.js
+++ LeetCode/53-Maximum_Subarray.js
@@ -3,19 +3,18 @@
 */
 
 var maxSubArray = function(nums) {
-  
+
   let maxSum = nums[0];
   let maxEndingHere = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-   
+
     maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
-    maxSum = Math.max(maxSum, maxEndingHere);  
+    maxSum = Math.max(maxSum, maxEndingHere);
   }
 
   return maxSum;
 }
-
 
 //let nums = [-1];  // Corner Case: -1
 let nums = [1,-3,4,-1,2,1,-5,4];  // 6
