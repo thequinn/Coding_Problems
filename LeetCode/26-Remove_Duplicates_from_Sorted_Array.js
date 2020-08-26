ba915e1e5327bed8d78f53096e18e5f8c9611b43
thequinn <annasun.usa@gmail.com>
2020-08-26 13:36:45 -0700

    Update 26-Remove_Duplicates_from_Sorted_Array.js

--- LeetCode/26-Remove_Duplicates_from_Sorted_Array.js
+++ LeetCode/26-Remove_Duplicates_from_Sorted_Array.js
@@ -5,8 +5,8 @@
 var removeDuplicates = function(nums) {
   if (nums.length == 0)   return 0;
 
-  let i = 0;  // i is slow ptr
-  for (let j = 1; j < nums.length; j++) {  // j is fast ptr
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
     if (nums[j] != nums[i]) {
       i++;
       nums[i] = nums[j];
@@ -17,8 +17,8 @@ var removeDuplicates = function(nums) {
   return ++i;
 };
 
-let nums = [];  // 0
-//let nums = [1,1,2];                // 2, b/c [1,2]
+//let nums = [];  // 0
+let nums = [1,1,2];                // 2, b/c [1,2]
 //let nums = [1,2,2,2,3]             // 3, b/c [1,2,3]
 //let nums = [0,0,1,1,1,2,2,3,3,4];  // 5, b/c [0,1,2,3,4]
 console.log(removeDuplicates(nums));
