8a59221eb7bde899f573874185a22cfc4a735107
thequinn <annasun.usa@gmail.com>
2020-08-27 10:04:13 -0700

    Update 122-Best_Time_to_Buy_and_Sell_Stock_II.js

--- LeetCode/122-Best_Time_to_Buy_and_Sell_Stock_II.js
+++ LeetCode/122-Best_Time_to_Buy_and_Sell_Stock_II.js
@@ -2,24 +2,28 @@
 122. Best Time to Buy and Sell Stock II
 */
 
-var maxProfit = function(prices) {
-  let i = 0, profit = 0;
+let maxProfit = function(prices) {
+  let i = 0, maxProfit = 0;
+  let valley = prices[0], peak = prices[0];
 
-  for (let j = i+1; j < prices.length; ) {
-
-    if (prices[i] > prices[j]) {
-      i = j; j++;
+  while (i < prices.length - 1) {
+    while (i < prices.length - 1 && prices[i] >= prices[i + 1]) {
+      i++;
     }
+    valley = prices[i];
 
-    if (prices[i] < prices[j]) {
-      profit = profit + prices[j] - prices[i];
-      console.log("profit:", profit);
-      i = j + 1;
-      j = i + 1;
+    while (i < prices.length - 1 && prices[i] <= prices[i + 1]) {
+      i++;
     }
+    peak = prices[i];
+
+    maxProfit += peak - valley;
   }
-  return profit;
-};
+
+  return maxProfit;
+}
+
+
 
 let prices = [7,1,5,3,6,4];
 let result = maxProfit(prices);
