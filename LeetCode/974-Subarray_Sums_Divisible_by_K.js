9a047ece52588821dce191ef773d0c9545eec0ca
thequinn <annasun.usa@gmail.com>
2020-09-24 12:49:56 -0700

    done

--- LeetCode/974-Subarray_Sums_Divisible_by_K.js
+++ LeetCode/974-Subarray_Sums_Divisible_by_K.js
@@ -3,5 +3,15 @@
 */
 
 var subarraysDivByK = function(A, K) {
-
+  A.unshift(0);
+  return A
+    .map((sum => value => sum += value)(0))
+    .reduce((a, c) => {
+      a[(c % K + K) % K]++;
+      return a;
+    }, new Array(K).fill(0))
+    .reduce((a,c) => {
+      a += (c * (c-1) / 2) >> 0;
+      return a;
+    }, 0);
 };
