b9da15bb881145cd26b2df5f8424737b237d1b90
thequinn <annasun.usa@gmail.com>
2020-10-21 11:04:43 -0700

    Update 198-House_Robber.js

--- LeetCode/198-House_Robber.js
+++ LeetCode/198-House_Robber.js
@@ -4,20 +4,20 @@
 
 const rob = (nums) => {
   if (nums.length == 0) return 0;
-  
+
   let prev1 = 0;
   let prev2 = 0;
-  
+
   for (let num of nums) {
     let tmp = prev1;
     prev1 = Math.max(prev2 + num, prev1);
     prev2 = tmp;
   }
-  
+
   return prev1;
 };
 
-
+//-----Test Cases-----
 //let nums = [1,2,3,1];
 let nums = [2,7,9,3,1];
 console.log(rob(nums));
